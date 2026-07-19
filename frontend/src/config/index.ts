50fc757f1c177184e8424f139303538af03801e2
pepasse <102338652+pepasse@users.noreply.github.com>
2026-07-19 13:22:44 +0100

    Ajouter: Index pour exporter les configurations frontend (index.ts)

---
+++ frontend/src/config/index.ts
@@ -0,0 +1,3 @@
+export { API_CONFIG, MAPBOX_CONFIG, ENV, APP_CONFIG, AUTH_CONFIG, UI_CONFIG, FEATURE_FLAGS } from './config';
+export { HTTP_STATUS, ENDPOINTS, ERROR_MESSAGES, SUCCESS_MESSAGES } from './constants';
+export { default as apiClient } from './apiClient';
